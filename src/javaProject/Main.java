dc25364f67011609066e13df3bdb1191ef36641e
RafiAhmad <ahmadrafi025@gmail.com>
2022-09-30 21:12:40 +0530

    Initial commit

---
+++ src/javaProject/Main.java
@@ -0,0 +1,11 @@
+package javaProject;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Testing");
+
+        MyWindow w = new MyWindow();
+
+    }
+}
